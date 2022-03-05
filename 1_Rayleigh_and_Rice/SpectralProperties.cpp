3e6730c480ab98487ae9dabdb7708c4507f0e7b4
Wilker-Oliveira <wilkerfeitosa.wf@gmail.com>
2022-03-04 22:57:54 -0300

    Changes to be committed: 	modified:   SpectralProperties.cpp(gaussian function finished)

--- 1_Rayleigh_and_Rice/SpectralProperties.cpp
+++ 1_Rayleigh_and_Rice/SpectralProperties.cpp
@@ -13,3 +13,17 @@ double SpectralProperties::GaussianPSD(long long freq, double std_dev, double fc
 
   return (var/fc)*AmpConst*exp_part;
 }
+
+double SpectralProperties::GaussianACF(short tau, double std_dev, double fc){
+  double var=0, ArgConst=0;
+
+  var=pow(std_dev, 2);
+  ArgConst=(M_PI*fc*tau)/sqrt(log(2));
+
+  return var*exp(-pow(ArgConst, 2));
+}
+
+double SpectralProperties::GaussianBeta(double std_dev, double fc){
+
+  return 2*pow(M_PI*fc*std_dev, 2)/log(2);
+}
