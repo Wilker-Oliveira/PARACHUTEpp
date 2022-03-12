2162b091da41194dc17237c765ccca376e3e694e
Wilker-Oliveira <wilkerfeitosa.wf@gmail.com>
2022-03-12 15:10:18 -0300

    Changes to be committed: 	deleted:    1_Rayleigh_and_Rice/PSD_test.csv 	deleted:    1_Rayleigh_and_Rice/PSDplot.py 	deleted:    1_Rayleigh_and_Rice/PSDtest.cpp

--- 1_Rayleigh_and_Rice/SpectralProperties.cpp
+++ 1_Rayleigh_and_Rice/SpectralProperties.cpp
@@ -2,6 +2,38 @@
 
 //the Oth-order Bessel function of the first kind.
 
+//Numerical Integration form (one third of simpson)
+double SpectralProperties::besselJ0(double x, float step=0.01){
+  double I=0,s1=0,s2=0,s3=0;
+
+  auto f=[](double x,double o){ return cos(x*cos(o)); };
+
+  for(float i=step;i<(M_PI_2-step);i+=step){
+    if(int(i/step)%2!=0) s2+=f(x,i);
+    else s3+=f(x,i);
+  }
+  
+  s1=f(x,0)+f(x,M_PI_2);
+
+  I=(s1+4*s2+2*s3)*(step/3);
+  
+  return M_2_PI*I;
+};
+
+//Truncated infinite power-series form
+double SpectralProperties::besselJ0(double x, int t=4){
+  double J0=1,x2=x*x,prod=1;
+  
+  for(int i=1;i<=t;i++){
+    prod*=pow((2*i),2);
+    J0+=pow(-x2,i)/prod;
+  }
+
+  return J0;
+}
+
+
+
 //Jakes Equivalent part 
 double SpectralProperties::jakesPSD(double freq, double std_dev, double fmax){
   double var=0;
@@ -15,6 +47,20 @@ double SpectralProperties::jakesPSD(double freq, double std_dev, double fmax){
     return var/(M_PI*fmax*sqrt(1-pow(freq/fmax,2)));
   }
   return 0;
+}
+
+double SpectralProperties::jakesACF(float tau, double std_dev, double fmax){
+ double var=0,arg=0;
+ var=pow(std_dev,2);
+
+ arg=2*M_PI*fmax*tau;
+
+ //limtis's choose based on empirical test
+ if(arg>=-3 && arg <3.1) 
+   return var*besselJ0(arg,4);
+ 
+ return var*besselJ0(arg,0.01f);
+
 }
 
 double SpectralProperties::jakesBeta(double std_dev, double fmax){
@@ -34,7 +80,7 @@ double SpectralProperties::GaussianPSD(double freq, double std_dev, double fc){
   return (var/fc)*AmpConst*exp_part;
 }
 
-double SpectralProperties::GaussianACF(short tau, double std_dev, double fc){
+double SpectralProperties::GaussianACF(float tau, double std_dev, double fc){
   double var=0, ArgConst=0;
 
   var=pow(std_dev, 2);
--- 1_Rayleigh_and_Rice/SpectralProperties.hpp
+++ 1_Rayleigh_and_Rice/SpectralProperties.hpp
@@ -9,14 +9,16 @@ class SpectralProperties{
 
 public:
 
-  //also inplement: the Oth-order Bessel function of the first kind.
+  //the Oth-order Bessel function of the first kind.
+  static double besselJ0(double x, float step);
+  static double besselJ0(double x, int t /*Number of terms*/);
 
   static double jakesPSD(double freq, double std_dev, double fmax /*Maximum Doppler frequency*/);
-  static double jakesACF(short int tau, double std_dev, double fmax /*Maximum Doppler frequency*/);
+  static double jakesACF(float tau, double std_dev, double fmax /*Maximum Doppler frequency*/);
   static double jakesBeta(double std_dev, double fmax /*Maximum Doppler frequency*/);
 
   static double GaussianPSD(double freq, double std_dev, double fc /*3-dB-cut-off frequency*/);
-  static double GaussianACF(short int tau, double std_dev, double fc /*3-dB-cut-off frequency*/);
+  static double GaussianACF(float tau, double std_dev, double fc /*3-dB-cut-off frequency*/);
   static double GaussianBeta(double std_dev, double fc /*3-dB-cut-off frequency*/);
 };
 
