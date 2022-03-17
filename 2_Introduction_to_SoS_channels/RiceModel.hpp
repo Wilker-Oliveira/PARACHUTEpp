21881f03550df3a8832508e98a27cdfe59af69b1
Wilker-Oliveira <wilkerfeitosa.wf@gmail.com>
2022-03-17 20:51:49 -0300

    Changes to be committed: 	new file:   2_Introduction_to_SoS_channels/RiceModel.hpp

---
+++ 2_Introduction_to_SoS_channels/RiceModel.hpp
@@ -0,0 +1,27 @@
+//Implementation of the Rice model class
+
+#ifndef RICEMODEL_HPP
+#define RICEMODEL_HPP
+
+#include <complex>
+
+class RiceModel{
+
+private:
+
+  double *PathGains;
+  double *DopplerFrequencies;
+  double *Phases;
+
+public:
+  
+  RiceModel();//set the attributes to nullptr
+  ~RiceModel();//deletes the attributes
+
+  virtual void ModelEstimate(short L/*Number of multipaths*/, float K /*Rice factor in dB*/, double fmax) = 0;
+
+  std::complex<double> * Channel(float *t);
+
+};
+
+#endif
