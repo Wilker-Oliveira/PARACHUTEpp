aa17a9c571ad3d1e547eb46d0ae865aa27115e93
mclararl <clara@gtel.ufc.br>
2022-11-11 12:46:50 -0300

    Changes to be committed: 	modified:   MSEMtest.cpp

--- MRC_1_IS/Test/MSEMtest.cpp
+++ MRC_1_IS/Test/MSEMtest.cpp
@@ -5,11 +5,38 @@
 #include <algorithm>
 #include <iterator>
 #include <vector>
+#include <complex>
 #include "../Models/MSEModel.hpp"
 
 using namespace std;
 
-//double AutoCorrelation(); 
+vector<complex<float>> autoCorr(vector<complex<float>> u, float lag){
+  complex<float> aux (0.0, 0.0);
+  vector<complex<float>> ac;
+  vector<complex<float>> h;
+  copy(u.begin(), u.end(), back_inserter(h));
+  //reverse(h.begin(), h.end());
+
+  for(float i = 0; i < lag; i++){
+    for(float j = 0; j < lag-i; j++){
+      aux += u[j]*conj(h[j+i]);
+    }
+    ac.push_back(aux);
+    aux = 0;
+  }
+  return ac;
+}
+
+vector<float> envACF(float sig, vector<float> acf){
+  vector<float> en_acf;
+  float aux;
+  
+  for(float i = 0; i< acf.size(); i++){
+    aux = pow(sig, 2)*(M_PI_2)*(1 + (std::pow<float>(acf[i],2)/(4*pow(sig,4))));
+    en_acf.push_back(aux);
+  }
+  return en_acf;
+}
 
 int main(){
 
@@ -25,50 +52,40 @@ int main(){
   ostream_iterator<string> out_it (OutFile,";");
 
   MSEModel<N,float> u1(0.7071,fmax),u2(0.7071,fmax);
-  vector<float> time((tf-ti)/dt);
-  vector<float> ACFvec((tf-ti)/dt);
+  vector<float> time((10*tc)/(dt*2));
+  vector<float> T_ACFPvec((10*tc)/(dt*2));
 
-    for(float i=0;i<(tf-ti)/dt;i++){
+  for(float i=0;i<(10*tc)/(dt*2);i++){
       time[i] = ti + i*dt;
-      ACFvec[i]=u2.CalcACF(time[i]);
+      T_ACFPvec[i]=u2.CalcACF(time[i]);
     }
-  
+  vector<float> T_ACFvec = envACF(0.7071, T_ACFPvec);
   vector<float> u1value = u1.CalcProcess(time);
   vector<float> u2value = u2.CalcProcess(time);
-  vector<float> aux1,aux2;
+  vector<complex<float>> uvalue;
+  complex<float> ch;
 
-  for(int j=0; j<10000; j++){
-    u1.genPhases();
-    u2.genPhases();
-    aux1=u1.CalcProcess(time);
-    aux2=u2.CalcProcess(time);
-    for(float k=0;k<(tf-ti)/dt;k++){
-      u1value[k] = aux1[k] + u1value[k];
-      u2value[k] = aux2[k] + u2value[k];
-    }
-    aux1.clear();
-    aux2.clear();
+  for(int i = 0; i < u1value.size(); i++){
+    ch = complex<float>(u1value[i], u2value[i]);
+
+    uvalue.push_back(ch);
+    
+    //uvalue.push_back(complex<float>(u1value[i], u2value[i]));
   }
 
-  for(float k=0;k<(tf-ti)/dt;k++){
-    u1value[k] /= 10000;
-    u2value[k] /= 10000;
-  }
-
-  
+  vector<complex<float>> S_ACFvec = autoCorr(uvalue,(10*tc)/(dt*2));
   OutFile.open("RayleighChannelMSEM.csv", ios::out | ios::trunc);
 
   OutFile<<"time"<<';';
-  OutFile<<"ACF"<<';';
-  OutFile<<"u1"<<';';
-  OutFile<<"u2"<<"\n";
+  OutFile<<"Theoretical_ACF"<<';';
+  OutFile<<"Empiric_ACF"<<"\n";
 
 
-  for(float i=0;i<(tf-ti)/dt;i++){
+
+  for(float i=0;i<(10*tc)/(dt*2);i++){
     OutLine.push_back(to_string(time[i]));
-    OutLine.push_back(to_string(ACFvec[i]));
-    OutLine.push_back(to_string(u1value[i]));
-    OutLine.push_back(to_string(u2value[i]));
+    OutLine.push_back(to_string(T_ACFvec[i]));
+    OutLine.push_back(to_string(abs(S_ACFvec[i])/S_ACFvec.size()));
     copy(OutLine.begin(),OutLine.end()-1,out_it);
     OutFile<<OutLine.back()<<'\n';
     OutLine.clear();
